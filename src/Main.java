22a8c73ff10e2f4ff948775e7161259dcd28008e
BT_2S14_21 <kmei0013@yahoo.com>
2025-01-28 09:51:40 -0500

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,27 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        File f = new File("src/input_file");
+
+        String fileData = "";
+        try {
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) {
+                String currentLine = s.nextLine();
+                fileData += currentLine + "\n";
+            }
+
+            // a String array where every item in the array is a line from the file
+            String[] fileArray = fileData.split("\n");
+            System.out.println(Arrays.toString(fileArray));
+        } catch (FileNotFoundException fe) {
+            System.out.println("File was not found");
+            System.exit(1);
+        }
+        System.out.println(fileData);
     }
 }
